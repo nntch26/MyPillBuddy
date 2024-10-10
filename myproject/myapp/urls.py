166f174dc534c9c4aa9c2954c5da5055939e6a2f
Teset <158586761+iPhum77max@users.noreply.github.com>
2024-10-10 18:27:45 +0700

    login register

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -6,4 +6,7 @@ from .views import *
 
 urlpatterns = [
     path("", indexView.as_view(), name="index"),
+    path("register/", RegisterView.as_view(), name="url_register"),
+    path("login/", LoginView.as_view(), name="url_login"),
+    path("logout/", LogoutView.as_view(), name="url_logout"),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import logout, login, authenticate, update_session_auth_hash
 
 class indexView(View):
     def get(self, request):
         return render(request, 'index.html')
-# Create your views here.
+
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'register.html', {'form': form})
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('url_login')
+        return render(request, 'register.html', {'form': form})
+    
+class LoginView(View):
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request, 'login.html')
+    def post(self, request):
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            user = form.get_user() 
+            login(request,user)
+            return redirect('index')
+        return render(request, 'login.html', {'form': form})
+    
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('url_login')
